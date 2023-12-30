c02673955977111eaf9d925ab7ae475ecfe2fa7d
chiragsharma21 <75730835+chiragsharma21@users.noreply.github.com>
2023-12-30 12:17:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_frozen import Freezer
+import requests
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+@app.route('/')
+def index():
+    url = "https://s3.amazonaws.com/open-to-cors/assignment.json"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        products = data.get("products", {})
+        sorted_products = sorted(products.values(), key=lambda x: int(x["popularity"]), reverse=True)
+        return render_template('index.html', products=sorted_products)
+    else:
+        return f"Failed to fetch data. Status code: {response.status_code}"
+
+if __name__ == '__main__':
+    freezer.freeze()
+    app.run(debug=True)
